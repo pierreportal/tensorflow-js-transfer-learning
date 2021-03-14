96c0f54b3b0743bff6713d5784631213748ff416
Pierre Portal <pierreportal.mac@gmail.com>
2021-03-14 14:41:10 +0100

    fix: cam stream

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,35 +1,20 @@
 let net;
+let webcam;
 
 const webcamElement = document.getElementById("webcam");
 const classifier = knnClassifier.create();
 
 async function app() {
   console.log("Loading mobilenet..");
-
-  // Load the model.
   net = await mobilenet.load();
   console.log("Successfully loaded model");
 
-  const webcam = await tf.data.webcam(webcamElement);
-  console.log("webcam: ", webcam);
-  // Reads an image from the webcam and associates it with a specific class
-  // index.
   const addExample = async (classId) => {
-    // Capture an image from the web camera.
     const img = await webcam.capture();
-
-    // Get the intermediate activation of MobileNet 'conv_preds' and pass that
-    // to the KNN classifier.
     const activation = net.infer(img, true);
-
-    // Pass the intermediate activation to the classifier.
     classifier.addExample(activation, classId);
-
-    // Dispose the tensor to release the memory.
     img.dispose();
   };
-
-  // When clicking a button, add an example for that class.
   document
     .getElementById("class-a")
     .addEventListener("click", () => addExample(0));
@@ -43,14 +28,9 @@ async function app() {
   while (true) {
     if (classifier.getNumClasses() > 0) {
       const img = await webcam.capture();
-
-      // Get the activation from mobilenet from the webcam.
       const activation = net.infer(img, "conv_preds");
-      // Get the most likely class and confidence from the classifier module.
       const result = await classifier.predictClass(activation);
-
       const classes = ["A", "B", "C"];
-
       document.getElementById("console").innerText =
         result.confidences[result.label] === 1
           ? `
@@ -58,26 +38,24 @@ async function app() {
           probability: ${result.confidences[result.label]}
         `
           : "";
-
-      // Dispose the tensor to release the memory.
       img.dispose();
     }
 
     await tf.nextFrame();
   }
 }
-// navigator.mediaDevices.enumerateDevices().then(gotDevices);
-
 const constraints = {
   video: {
-    // facingMode: { exact: "user" },
+    facingMode: "environment",
   },
+  audio: false,
 };
 
 // Activate the webcam stream.
-navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+navigator.mediaDevices.getUserMedia(constraints).then(async function (stream) {
+  console.log("stream: ", stream);
   webcamElement.srcObject = stream;
-  // webcamElement.addEventListener("loadeddata", app);
+  webcam = await tf.data.webcam(webcamElement);
   app();
 });
 
